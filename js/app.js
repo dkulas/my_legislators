198fb5992b6da8d31073527e6e3346bf890bfd42
Danny Kulas <dkulas@cogentibs.com>
2017-05-11 12:06:35 -0400

    added text shadow to main heading

--- js/app.js
+++ js/app.js
@@ -46,7 +46,6 @@ function fetchData() {
         resultsList.innerHTML += "<li><strong>Facebook</strong>: <a href=https://www.facebook.com/"  + dataResults[i]["facebook_id"] +  ">Facebook</a></li>";
         resultsList.innerHTML += "<li><strong>Twitter</strong>: <a href=https://www.twitter.com/@"  + dataResults[i]["twitter_id"] +  ">Twitter</a></li>";
         resultsList.innerHTML += "<li><br></li>";
-        
         resultsList.style.display = "inline-block";
       };
     } else {
